f0bdce19434138788abb1d572cf2e67e0b0d1461
Muato <rufuss18@inbox.lv>
2018-08-01 22:19:53 +0300

    Unit stat began

---
+++ Assets/Scripts/UnitScripts/UnitStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum UnitClasses
+{
+	Peasent,
+	Warrior,
+	Archer,
+	Knight,
+	Thief,
+	Priest,
+	Wizard,
+	Blademaster
+};
+
+public class UnitStats {
+
+	// Unit stats
+	public int ID;
+	public int UnitID;
+	public string Name;
+
+	UnitClasses unitClass { get; set; }
+
+	int Strength;
+	int Vitality;
+	int Agility;
+	int Dexterity;
+	int Intelligence;
+	int Wisdom;
+	int Charisma;
+
+
+
+}
